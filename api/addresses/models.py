531f95cd7deacb9d9260ce2807b5801786ff15d4
Hugo Sobral <hugosobral10@gmail.com>
2020-10-13 10:51:16 -0300

    Create address model

--- api/addresses/models.py
+++ api/addresses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Adress(models.Model):
+    line1 = models.CharField(max_length=150)
+    line2 = models.CharField(max_length=150)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    lat = models.FloatField(null=True, blank=True)
+    long = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.line1
